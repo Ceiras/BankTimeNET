f19f8bbb8766629a581c9855f54c31e88e197e5d
Cristina Eiras Rodríguez <ceiras@uoc.edu>
2021-12-29 23:18:55 +0100

    Adds login and singup form

---
+++ views/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BankTimeNET.views
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            String username = this.usernameInput.Text;
+            String password = this.passwordInput.Password;
+
+            MessageBox.Show($"Username: {username} - {password}");
+        }
+
+        private void singupButton_Click(object sender, RoutedEventArgs e)
+        {
+            loginFrame.Navigate(new SingUp());
+        }
+    }
+}
---
+++ views/SingUp.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BankTimeNET.views
+{
+    /// <summary>
+    /// Lógica de interacción para SingUp.xaml
+    /// </summary>
+    public partial class SingUp : Page
+    {
+        public SingUp()
+        {
+            InitializeComponent();
+        }
+        private void singUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            singupFrame.Navigate(new Login());
+        }
+    }
+}
