dd71a1ee8939952f928b9dfdb22cba02ca665969
Cristina Eiras Rodríguez <ceiras@uoc.edu>
2021-12-30 20:57:37 +0100

    Adds DB Connection

---
+++ db/Db.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace BankTimeNET.db
+{
+    public static class Db
+    {
+        public static SqlConnection connect()
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["BankTimeNETConnection"].ConnectionString;
+            sqlConnection.Open();
+
+            return sqlConnection;
+        } 
+    }
+}
